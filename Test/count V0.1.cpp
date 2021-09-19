abe062406af63af92a5030e73b76842ef56f0db5
hacknetxiaolan <54237644+hacknetxiaolan@users.noreply.github.com>
2021-09-19 20:57:18 +0800

    Add files via upload

---
+++ Test/count V0.1.cpp
@@ -0,0 +1,106 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+const string KeyWords[32] = {
+    "auto", "break", "case","char",
+    "const", "continue", "default", "do",
+    "double", "else", "enum", "extern",
+    "float", "for", "goto", "if",
+    "int", "long", "register", "return",
+    "short", "signed", "sizeof", "static",
+    "struct", "switch", "typedef", "union",
+    "unsigned", "void", "volatile", "while"
+};
+
+struct countdata{
+    int cttotal = 0;    //总和
+    int ctsw = 0;       //switch
+    int ctcase = 0;     //cast
+    int ctie = 0;       //if-else
+    int ctieie = 0;     //if-else if-else
+};
+
+void AddressAdjustment(string add){
+    int s = add.length();
+    stack <char> fn;
+
+    while (s--)
+      fn.push(add[s]);//现在，堆栈为完整的地址
+    
+    add = "";
+
+    while (!fn.empty()){
+        if (fn.top() == '\\')//由于语言本身问题，双斜杠本质上是表示单斜杠
+        add += '\\';
+
+        else
+        add += fn.top();
+
+        fn.pop();
+    }
+}
+
+string ReadFile(string add){
+    fstream file;
+    stringstream buf;
+
+    file.open(add,ios::in);
+    buf << file.rdbuf();
+    file.close();
+    return buf.str();
+}
+
+void Matching (string temp, countdata &result){//用途：进行关键词库的比对
+    if (temp.size() == 1)
+    return;
+    
+    for (int i = 0; i < 32; i++){
+        if (temp == KeyWords[i]){
+            if (temp == "switch")
+                result.ctsw++;
+            else if (temp == "case")
+                result.ctcase++;
+            else if (temp == "double")
+                result.cttotal--;//验证double成功时，其实在之前已经当成"do"验证过一次。
+            result.cttotal++;
+            break;
+        }
+    }
+    return;
+}
+
+void analysize(string data, countdata &result){//用途：开始分析单词，将单词送入Matching进行比对
+    int l = data.length();
+    string temp = "\0";
+
+    for (int i = 0; i < l; i++){
+        if (isalpha(data[i])){
+            temp += data[i];
+            Matching(temp, result);
+            }
+        else
+            temp = "\0";
+    }
+}
+
+int main(){
+
+    string add;   //地址
+    int  level;    //完成等级
+
+    cout << "Please inpress the address (including suffix) and the dealing level" << endl;
+    cin >> add >> level;
+
+    AddressAdjustment(add);//解决输入地址为单斜杠的问题
+    
+    string data;//处理的读入数据
+    data = ReadFile(add);
+
+    countdata result;
+    analysize(data, result);
+    //下一行测试
+    cout<<result.cttotal<<endl<<result.ctsw<<endl<<result.ctcase<<endl;
+
+    system("pause");
+    return 0;
+}   
